2078f208ad6a8b2c11ffb0baa5a0e5a169ced172
Olaf <olaf.chuszno@gmail.com>
2023-12-13 20:56:40 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// person = {
+//     name: 'olaf', 
+//     surname: 'ch', 
+//     age: 20, 
+//     address: {
+//         street: 'bogdana 14',
+//         zip_code: '02-789',
+//         city: true
+//     }
+// }
+
+// let colors = ['orange', 'red', 'blue', 'pink', 'cyan'];
+
+// const colorsMap = colors.map((color, index) => {
+//     return console.log(`${index}: ${color}`);
+// });
+
+// function sayHello() {
+//     let greeting = 'hello';
+//     var message = 'hi!';
+
+//     console.log(greeting);
+//     console.log(message);
+
+//     return;
+// }
+
+// sayHello();
+
+
+
+let a = 2;
+let b = 2;
+
+console.log(a === b);
