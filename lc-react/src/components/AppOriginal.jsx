91f0c2906aa90baa06e44abba41b43146575b039
Olaf <olaf.chuszno@gmail.com>
2023-12-19 22:03:06 +0300

    Create new app.jsx

--- lc-react/src/components/AppOriginal.jsx
+++ lc-react/src/components/AppOriginal.jsx
@@ -1,44 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { Component, useState } from "react";
-import Another from "./Another.jsx";
+import { useState } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   function decrement() {
-    setCount((prevCount) => prevCount - 1);
+    setCount(prevCount => prevCount - 1);
   }
 
   function increment() {
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
   }
 
   const someStyle = {
-    background: "blue",
-    color: "white",
-    fontSize: "28px",
-    fontWeight: "bold",
+    background: 'blue',
+    color: 'white',
+    fontSize: '28px',
+    fontWeight: 'bold',
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Another id="anotherId" name="anotherName" />
         <div>
           <span>{count}</span>
-          <button type="button" onClick={decrement}>
-            -
-          </button>
-          <button type="button" onClick={increment}>
-            +
-          </button>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
+        {true && <p style={someStyle}>{3 + 2}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
