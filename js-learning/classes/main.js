7ca65f20b3e4ddc12a4a5528400d47cec44caed0
Olaf <olaf.chuszno@gmail.com>
2024-01-06 21:01:38 +0300

    After classes lesson

--- js-learning/classes/main.js
+++ js-learning/classes/main.js
@@ -4,7 +4,40 @@ class Item {
     this.category = category;
   }
 
+  static maxItems = 10;
+
+  static getHelpersText() {
+    return "Add items to your shopping list.";
+  }
+
   getDetails() {
     return `${this.name} - ${this.category}`;
   }
 }
+
+// Extends the Item class
+class PurchasedItem extends Item {
+  constructor(name, category, price) {
+    super(name, category);
+    this.price = price;
+  }
+
+  getDetailsWithPrice() {
+    return `${this.name} - ${this.category} - ${this.price}`;
+  }
+
+  static getNumberOfItems() {
+    return `3 / ${super.maxItems}`;
+  }
+}
+
+let item = new Item("coffee", "food");
+item.category = "drink";
+
+let purchasedItem = new PurchasedItem("sugar", "food", "2.49");
+
+document.getElementById("output").innerHTML = item.getDetails();
+
+console.log(PurchasedItem.maxItems);
+
+console.log(PurchasedItem.getNumberOfItems());
