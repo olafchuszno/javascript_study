ad7e7cd1d371b095ba20b0f59fc3d6a22a372cf7
Olaf <olaf.chuszno@gmail.com>
2024-01-07 15:56:06 +0300

    Finished declarative methods leson

--- js-learning/declarative-methods/main.js
+++ js-learning/declarative-methods/main.js
@@ -1,4 +1,5 @@
 "use strict";
+const output = document.getElementById("output");
 
 // What declarative means?
 // We tell the compiler at runtime what we want the result to be
@@ -10,8 +11,49 @@
 let fruit = ["Bananas", "Strawberry", "Orange", "Apple", "Grapes", "Plum"];
 
 let fruitThatContainE = [];
-fruit.forEach((fruit) => {
-  if (fruit.contains("e")) {
-    fruitThatContainE.push(fruit);
-  }
-});
+
+// To check whether  string contains a character, use .includes() method
+// fruit.forEach(function (fruit) {
+//   if (fruit.includes("e")) {
+//     fruitThatContainE.push(fruit);
+//   }
+// });
+
+// We can achieve the same result by filtering with .filter() on a list
+
+fruitThatContainE = fruit.filter((f) => f.includes("e"));
+
+// To print out the array content we can use .join() method with a separator as the argument
+output.innerHTML = fruitThatContainE.join(", ");
+
+let users = [
+  {
+    name: "olaf",
+    email: "olaf@email.com",
+    stars: 5,
+  },
+  {
+    name: "zizi",
+    email: "zizi@email.com",
+    stars: 9,
+  },
+  {
+    name: "meme",
+    email: "meme@email.com",
+    stars: 1,
+  },
+];
+
+// Reduce method gives us basically a loop with a counter variable
+// which we can manipulate. The second value is a default for the accumulator.counter
+let totalStars = users.reduce((stars, user) => (stars += user.stars), 0);
+
+output.innerHTML += "<br>" + totalStars;
+
+//  MAPS
+
+// Loops over items in the array.
+// It expects an inline, anonymous function
+let userEmails = users.map((user) => user.email);
+
+output.innerHTML = "<br>" + userEmails.join(", ");
